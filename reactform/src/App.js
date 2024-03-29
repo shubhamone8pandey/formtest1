aabd1eece6b21428a7f01d19c5eafefe204a6a15
Shubham Pandey <135992950+shubhamone8pandey@users.noreply.github.com>
2024-03-29 13:34:02 +0530

    Update App.js

--- reactform/src/App.js
+++ reactform/src/App.js
@@ -8,7 +8,7 @@ const App = () => {
   const handleSubmit = (event) => {
     event.preventDefault()
     // Handle form submission here
-    axios.post('http://localhost:3001/api/users', {
+    axios.post('https://formtest-server.vercel.app/api/users', {
       name,
       email
     })
